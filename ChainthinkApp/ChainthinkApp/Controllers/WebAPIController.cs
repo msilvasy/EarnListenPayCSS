e27a2617406b944d8ff0353c859b0e2c7e4db953
manish sharma <manishsharma1086@outlook.com>
2026-04-22 13:52:53 +0530

    Changes

---
+++ ChainthinkApp/ChainthinkApp/Controllers/WebAPIController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChainthinkApp.Controllers
+{
+    public class WebAPIController : Controller
+    {
+        // GET: API
+     
+        public ActionResult SendEmail(string name, string email, string company, string message)
+        {
+
+            // We can write sending email code here.
+            return Json(new { Status = true, Message = "Message sent successfully!" });
+        }        
+    }
+}
---
+++ ChainthinkApp/ChainthinkApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ChainthinkApp.MvcApplication" Language="C#" %>
